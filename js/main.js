b80b6574a4973d43277c4b8aee93c735339ce1b9
Soraya Valle <svalleortega@gmail.com>
2020-06-02 18:06:31 +0200

    Add clock functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+
+const setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; // add degrees to compensate CSS rotate of hands. 
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
